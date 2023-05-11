a1f9d6d4a7be75e8236376a698ebe4a241e0d197
R-Morton <remann12@gmail.com>
2023-05-11 19:35:36 +1000

    switching between two form renders working.

--- src/components/MedicalForm.jsx
+++ src/components/MedicalForm.jsx
@@ -4,6 +4,9 @@ export default function MedicalForm(props) {
 
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
+  const [age, setAge] = useState("")
+  const [gender, setGender] = useState("")
+  const [currentIndex, setCurrentIndex] = useState(0)
 
   function handleChangeName (e) {
     setName(e.target.value)
@@ -13,18 +16,57 @@ export default function MedicalForm(props) {
     setEmail(e.target.value)
   }
 
-  const handleNext = (e) => {
+  function handleChangeAge (e) {
+    setAge(e.target.value)
+  }
+
+  function handleChangeGender (e) {
+    setGender(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
     e.preventDefault()
-    props.onNext({ name, email })
+    props.onSubmit({ name, email, age, gender })
+  }
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => prevIndex + 1);
+  };
+
+  const handlePrevious = () => {
+    setCurrentIndex((prevIndex) => prevIndex -1)
+  }
+
+  function form1() {
+    return(
+    <form>
+      <label>Name:</label>
+      <input type="text" value={name} onChange={handleChangeName} />
+      <label>Email:</label>
+      <input type="text" value={email} onChange={handleChangeEmail} />
+      <button type="button" onClick={handleNext}>Next</button>
+    </form>
+    )
+  }
+
+  function form2() {
+    return (
+    <form onSubmit={handleSubmit}>
+      <label>Age:</label>
+      <input type="text" value={age} onChange={handleChangeAge} />
+      <label>Gender:</label>
+      <input type="text" value={gender} onChange={handleChangeGender} />
+      <button type="button" onClick={handlePrevious}>Previous</button>
+      <button type="submit">Submit</button>
+    </form>
+    )
   }
 
     return (
-      <form onSubmit={handleNext}>
-          <label>Name:</label>
-          <input type="text" value={props.name} onChange={handleChangeName} />
-          <label>Email:</label>
-          <input type="text" value={props.email} onChange={handleChangeEmail} />
-          <button type="submit">Next</button>
-      </form>
+      <div>
+        {currentIndex === 0 && form1()}
+        {currentIndex === 1 && form2()}
+      </div>
+
     )
 }
--- src/components/MedicalParent.jsx
+++ src/components/MedicalParent.jsx
@@ -1,41 +1,25 @@
 import React, { useState } from 'react';
 import MedicalForm from './MedicalForm';
 import MedicalDisplay from './MedicalDisplay';
-import MedicalForm2 from './MedicalForm2';
 
 
 
 export default function MedicalParent() {
 
-    const [formData0, setFormData0] = useState("")
-    const [formData1, setFormData1] = useState("")
+    const [formData, setFormData] = useState("")
     const [showForm, setShowForm] = useState(false)
-    const [currentIndex, setCurrentIndex] = useState(0)
 
     const handleSubmit = (data) => {
-        setFormData1(data);
-        if (currentIndex === 1) {
-            setShowForm(true)
-        }
+        setFormData(data);
+        setShowForm(true)
     }
 
-    const handleNext = (data) => {
-        setCurrentIndex((prevIndex) => prevIndex + 1);
-        if (currentIndex === 0) {
-            setFormData0(data);
-        }
-      };
-    
-      const handlePrevious = () => {
-        setCurrentIndex((prevIndex) => prevIndex -1)
-      }
 
     return (
         <div>
             <h1>Form</h1>
-            {currentIndex === 0 && <MedicalForm onNext={handleNext} />}
-            {currentIndex === 1 && <MedicalForm2 onSubmit={handleSubmit} />}
-            {showForm && <MedicalDisplay name={formData0.name} email={formData0.email} age={formData1.age} gender={formData1.gender} />}
+            <MedicalForm onSubmit={handleSubmit}></MedicalForm>
+            {showForm && <MedicalDisplay name={formData.name} email={formData.email} age={formData.age} gender={formData.gender} />}
             
         </div>
     )
